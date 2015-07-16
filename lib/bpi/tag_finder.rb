18bf00d3cbacb03ef96010e1d9f66a3c248c3b6e
Samy KACIMI <samy.kacimi@gmail.com>
2015-07-16 16:52:43 +0200

    add tag finder method

---
+++ lib/bpi/tag_finder.rb
@@ -0,0 +1,68 @@
+module Bpi
+
+  def get_tag(object, tag)
+		case tag
+		when "impact_social"
+			if object.try(bpi_dictionary[:impact_social]).nil?
+				Rails.application.config.bpi.impact_social
+			else
+				object.send bpi_dictionary[:impact_social]
+			end
+		when "impact_environnemental"
+			if object.try(bpi_dictionary[:impact_environnemental]).nil?
+				Rails.application.config.bpi.impact_environnemental
+			else
+				object.send bpi_dictionary[:impact_environnemental]
+			end
+		when "impact_culturel"
+			if object.try(bpi_dictionary[:impact_culturel]).nil?
+				Rails.application.config.bpi.impact_culturel
+			else
+				object.send bpi_dictionary[:impact_culturel]
+			end
+		when "impact_eco"
+			if object.try(bpi_dictionary[:impact_eco]).nil?
+				Rails.application.config.bpi.impact_eco
+			else
+				object.send bpi_dictionary[:impact_eco]
+			end
+		when "categorie1"
+			if !object.try(bpi_dictionary[:categorie1]).nil?
+				object.send bpi_dictionary[:categorie1]
+			elsif Rails.application.config.bpi.categorie1.present?
+				Rails.application.config.bpi.categorie1
+			end
+		when "categorie2"
+			if !object.try(bpi_dictionary[:categorie2]).nil?
+				object.send bpi_dictionary[:categorie2]
+			elsif Rails.application.config.bpi.categorie2.present?
+				Rails.application.config.bpi.categorie2
+			end
+		when "mots_cles_op"
+			if !object.try(bpi_dictionary[:mots_cles_op]).nil?
+				object.send bpi_dictionary[:mots_cles_op]
+			elsif Rails.application.config.bpi.mots_cles_op.present?
+				Rails.application.config.bpi.mots_cles_op
+			end
+		when "mode_financement"
+			if object.try(bpi_dictionary[:mode_financement]).nil?
+				Rails.application.config.bpi.mode_financement
+			else
+				object.send bpi_dictionary[:mode_financement]
+			end
+		when "qualif_ESS"
+			if object.try(bpi_dictionary[:qualif_ESS]).nil?
+				Rails.application.config.bpi.qualif_ESS
+			else
+				object.send bpi_dictionary[:qualif_ESS]
+			end
+		when "ville"
+			if object.try(bpi_dictionary[:ville]).nil?
+				object.send bpi_dictionary[:ville]
+			end
+		else
+			nil
+		end
+	end
+
+end
