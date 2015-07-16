ab113acffae2ba258d0da64dbb3cbbabb0e1971d
Samy KACIMI <samy.kacimi@gmail.com>
2015-07-16 17:56:06 +0200

    correct ternaire

--- lib/bpi/tag_finder.rb
+++ lib/bpi/tag_finder.rb
@@ -3,13 +3,13 @@ module Bpi
   def get_tag(object, tag)
 		case tag
 		when "impact_social"
-			object.try(bpi_dictionary[:impact_social]).nil? Rails.application.config.bpi.impact_social : object.send bpi_dictionary[:impact_social]
+			object.try(bpi_dictionary[:impact_social]).nil? ? Rails.application.config.bpi.impact_social : object.send bpi_dictionary[:impact_social]
 		when "impact_environnemental"
-			object.try(bpi_dictionary[:impact_environnemental]).nil? Rails.application.config.bpi.impact_environnemental : object.send bpi_dictionary[:impact_environnemental]
+			object.try(bpi_dictionary[:impact_environnemental]).nil? ? Rails.application.config.bpi.impact_environnemental : object.send bpi_dictionary[:impact_environnemental]
 		when "impact_culturel"
-			object.try(bpi_dictionary[:impact_culturel]).nil? Rails.application.config.bpi.impact_culturel : object.send bpi_dictionary[:impact_culturel]
+			object.try(bpi_dictionary[:impact_culturel]).nil? ? Rails.application.config.bpi.impact_culturel : object.send bpi_dictionary[:impact_culturel]
 		when "impact_eco"
-			object.try(bpi_dictionary[:impact_eco]).nil? Rails.application.config.bpi.impact_eco : object.send bpi_dictionary[:impact_eco]
+			object.try(bpi_dictionary[:impact_eco]).nil? ? Rails.application.config.bpi.impact_eco : object.send bpi_dictionary[:impact_eco]
 		when "categorie1"
 			if !object.try(bpi_dictionary[:categorie1]).nil?
 				object.send bpi_dictionary[:categorie1]
@@ -29,9 +29,9 @@ module Bpi
 				Rails.application.config.bpi.mots_cles_op
 			end
 		when "mode_financement"
-			object.try(bpi_dictionary[:mode_financement]).nil? Rails.application.config.bpi.mode_financement : object.send bpi_dictionary[:mode_financement]
+			object.try(bpi_dictionary[:mode_financement]).nil? ? Rails.application.config.bpi.mode_financement : object.send bpi_dictionary[:mode_financement]
 		when "qualif_ESS"
-			object.try(bpi_dictionary[:qualif_ESS]).nil? Rails.application.config.bpi.qualif_ESS : object.send bpi_dictionary[:qualif_ESS]
+			object.try(bpi_dictionary[:qualif_ESS]).nil? ? Rails.application.config.bpi.qualif_ESS : object.send bpi_dictionary[:qualif_ESS]
 		when "ville"
 			object.send bpi_dictionary[:ville] unless object.try(bpi_dictionary[:ville]).nil?
 		else
