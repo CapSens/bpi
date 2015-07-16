9ee8a33b8b5e3f6899f011e00d8871c4cf3df60e
Samy KACIMI <samy.kacimi@gmail.com>
2015-07-16 18:50:52 +0200

    update gemspec

--- lib/bpi/builder.rb
+++ lib/bpi/builder.rb
@@ -4,6 +4,8 @@ module Bpi
 		Rails.application.config.bpi.dictionary
 	end
 
+	# data must be hash {url: 'https://toto.com/projet/', photo: 'https://toto.com/media/12'}
+
 	# XML Builder for ALL projects
 	def xml_history_builder(objects)
 		builder = Nokogiri::XML::Builder.new do |xml|
@@ -42,6 +44,52 @@ module Bpi
 			  		xml.montant_recherche object.send bpi_dictionary[:montant_recherche]
 			  		xml.montant_collecte object.send bpi_dictionary[:montant_collecte]
 					}
+				end
+		  }
+		end
+	end
+
+	# XML Builder for ongoing projects
+	def xml_current_builder(objects)
+		builder = Nokogiri::XML::Builder.new do |xml|
+		  xml.partenaire {
+				objects.map do |object|
+					data = object.to_bpi_data
+					ended = object.send bpi_dictionary[:time_ended?]
+					if !ended
+						xml.projet {
+				    	xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
+				    	xml.date_export Time.now.strftime("%Y-%m-%d")
+				    	xml.reference_projet object.send bpi_dictionary[:reference_projet]
+							xml.impact_social get_tag(object, "impact_social")
+							xml.impact_environnemental get_tag(object, "impact_environnemental")
+							xml.impact_culturel get_tag(object, "impact_culturel")
+							xml.impact_eco get_tag(object, "impact_eco")
+							if !get_tag(object, "categorie1").nil?
+				  			xml.categorie {
+									xml.categorie1 get_tag(object, "categorie1")
+									xml.categorie2 get_tag(object, "categorie2") unless get_tag(object, "categorie2").nil?
+				  			}
+							end
+							xml.mots_cles_nomenclature_operateur get_tag(object, "mots_cles_op") unless get_tag(object, "mots_cles_op").nil?
+							xml.mode_financement get_tag(object, "mode_financement")
+							xml.qualif_ESS get_tag(object, "qualif_ESS")
+							xml.code_postal object.send bpi_dictionary[:code_postal]
+				  		xml.ville get_tag(object, "ville") unless get_tag(object, "ville").nil?
+				  		xml.titre object.send bpi_dictionary[:titre]
+				  		xml.description object.send bpi_dictionary[:description]
+				  		xml.url data[:url]
+				  		xml.url_photo data[:photo]
+				  		fullstart = object.send bpi_dictionary[:date_debut_collecte]
+				  		xml.date_debut_collecte fullstart.strftime("%F")
+				  		fullend = object.send bpi_dictionary[:date_fin_collecte]
+				  		if fullend.to_s != ''
+				  			xml.date_fin_collecte fullend.strftime("%F")
+				  		end
+				  		xml.montant_recherche object.send bpi_dictionary[:montant_recherche]
+				  		xml.montant_collecte object.send bpi_dictionary[:montant_collecte]
+						}
+					end
 				end
 		  }
 		end
