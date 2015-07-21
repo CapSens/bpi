26b8f0820fa1f2386d0e05c60ef003edc3f43f01
Samy KACIMI <samy.kacimi@gmail.com>
2015-07-21 11:46:32 +0200

    hash case

--- lib/bpi/tag_finder.rb
+++ lib/bpi/tag_finder.rb
@@ -4,7 +4,7 @@ module Bpi
     if bpi_dictionary.has_key?(key) == false
       bpi_default[key]
     elsif object.bpi_dictionary[key].is_a? Hash
-      object.send bpi_dictionary[key][key]
+      object.send(hash.keys.first).send(hash.keys.first)
     else !object.bpi_dictionary[key].is_a? Hash
       object.send bpi_dictionary[key]
     end
