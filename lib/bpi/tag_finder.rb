8d5dff56a7d785b0760e506969f7d9e6af0adbd2
Samy KACIMI <samy.kacimi@gmail.com>
2015-07-22 13:53:56 +0200

    clean

--- lib/bpi/tag_finder.rb
+++ lib/bpi/tag_finder.rb
@@ -1,10 +1,11 @@
 module Bpi
 
   def get_tag(object, key)
-    if bpi_dictionary.has_key?(key) == false
+    if !bpi_dictionary.has_key?(key)
       bpi_default[key]
     elsif object.bpi_dictionary[key].is_a? Hash
-      object.send(hash.keys.first).send(hash.keys.first)
+      # object.send(hash.keys.first).send(hash.keys.first)
+      object.send(hash.fetch(key).values.first)
     else !object.bpi_dictionary[key].is_a? Hash
       object.send bpi_dictionary[key]
     end
