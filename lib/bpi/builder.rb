a3bc13a034bd43c4d9d34396d1c61ded026c38ad
Samy KACIMI <samy.kacimi@gmail.com>
2015-08-05 16:12:36 +0200

    rails exception logger

--- lib/bpi/builder.rb
+++ lib/bpi/builder.rb
@@ -16,69 +16,75 @@ module Bpi
 		builder = Nokogiri::XML::Builder.new do |xml|
 		  xml.partenaire {
 				objects.map do |object|
-					data = object.get_project_data
-					if xml_stream == "history" && object.try(bpi_dictionary['accept?']) == true
-			    	xml.projet {
-			    		xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
-			    		xml.date_export Time.now.strftime("%Y-%m-%d")
-			    		xml.reference_projet object.send bpi_dictionary['reference_projet']
-							xml.impact_social get_tag(object, 'impact_social')
-							xml.impact_environnemental get_tag(object, 'impact_environnemental')
-							xml.impact_culturel get_tag(object, 'impact_culturel')
-							xml.impact_eco get_tag(object, 'impact_eco')
-							if !get_tag(object, 'categorie1').nil?
-			  				xml.categorie {
-									xml.categorie1 get_tag(object, 'categorie1')
-									xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, "categorie2").nil?
-			  				}
-							end
-							xml.mots_cles_nomenclature_operateur get_tag(object, 'mots_cles_op') unless get_tag(object, 'mots_cles_op').nil?
-							xml.mode_financement get_tag(object, 'mode_financement')
-							xml.qualif_ESS get_tag(object, "qualif_ESS")
-							xml.code_postal object.send bpi_dictionary['code_postal']
-			  			xml.ville object.send bpi_dictionary['ville']
-			  			xml.titre object.send bpi_dictionary['titre']
-			  			xml.description object.send(bpi_dictionary['description']).gsub(/<\/?[^>]*>/,"") unless object.try(bpi_dictionary['description']).nil?
-			  			xml.url data[:url]
-			  			xml.url_photo data[:photo]
-			  			fullstart = object.send bpi_dictionary['date_debut_collecte']
-			  			xml.date_debut_collecte fullstart.strftime("%F")
-			  			fullend = object.send bpi_dictionary['date_fin_collecte']
-			  			xml.date_fin_collecte fullend.strftime("%F")
-			  			xml.montant_recherche object.send bpi_dictionary['montant_recherche']
-			  			xml.montant_collecte object.send bpi_dictionary['montant_collecte']
-						}
-					elsif xml_stream == "current" && object.try(bpi_dictionary['time_ended?']) == false
-						xml.projet {
-				    	xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
-				    	xml.date_export Time.now.strftime("%Y-%m-%d")
-				    	xml.reference_projet object.send bpi_dictionary['reference_projet']
-							xml.impact_social get_tag(object, 'impact_social')
-							xml.impact_environnemental get_tag(object, 'impact_environnemental')
-							xml.impact_culturel get_tag(object, 'impact_culturel')
-							xml.impact_eco get_tag(object, 'impact_eco')
-							if !get_tag(object, 'categorie1').nil?
-				  			xml.categorie {
-									xml.categorie1 get_tag(object, 'categorie1')
-									xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, 'categorie2').nil?
-				  			}
-							end
-							xml.mots_cles_nomenclature_operateur get_tag(object, 'mots_cles_op') unless get_tag(object, 'mots_cles_op').nil?
-							xml.mode_financement get_tag(object, 'mode_financement')
-							xml.qualif_ESS get_tag(object, 'qualif_ESS')
-							xml.code_postal object.send(bpi_dictionary['code_postal']) unless object.send(bpi_dictionary['code_postal']).empty?
-				  		xml.ville object.send bpi_dictionary['ville']
-				  		xml.titre object.send bpi_dictionary['titre']
-				  		xml.description object.send(bpi_dictionary['description']).gsub(/<\/?[^>]*>/,"") unless object.try(bpi_dictionary['description']).nil?
-				  		xml.url data[:url]
-				  		xml.url_photo data[:photo]
-				  		fullstart = object.send bpi_dictionary['date_debut_collecte']
-				  		xml.date_debut_collecte fullstart.strftime("%F")
-				  		fullend = object.send bpi_dictionary['date_fin_collecte']
-				  		xml.date_fin_collecte fullend.strftime("%F")
-				  		xml.montant_recherche object.send bpi_dictionary['montant_recherche']
-				  		xml.montant_collecte object.send bpi_dictionary['montant_collecte']
-						}
+					begin
+						data = object.get_project_data
+						if xml_stream == "history" && object.try(bpi_dictionary['accept?']) == true
+				    	xml.projet {
+				    		xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
+				    		xml.date_export Time.now.strftime("%Y-%m-%d")
+				    		xml.reference_projet object.send bpi_dictionary['reference_projet']
+								xml.impact_social get_tag(object, 'impact_social')
+								xml.impact_environnemental get_tag(object, 'impact_environnemental')
+								xml.impact_culturel get_tag(object, 'impact_culturel')
+								xml.impact_eco get_tag(object, 'impact_eco')
+								if !get_tag(object, 'categorie1').nil?
+				  				xml.categorie {
+										xml.categorie1 get_tag(object, 'categorie1')
+										xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, "categorie2").nil?
+				  				}
+								end
+								xml.mots_cles_nomenclature_operateur get_tag(object, 'mots_cles_op') unless get_tag(object, 'mots_cles_op').nil?
+								xml.mode_financement get_tag(object, 'mode_financement')
+								xml.qualif_ESS get_tag(object, "qualif_ESS")
+								xml.code_postal object.send bpi_dictionary['code_postal']
+				  			xml.ville object.send bpi_dictionary['ville']
+				  			xml.titre object.send bpi_dictionary['titre']
+				  			xml.description object.send(bpi_dictionary['description']).gsub(/<\/?[^>]*>/,"") unless object.try(bpi_dictionary['description']).nil?
+				  			xml.url data[:url]
+				  			xml.url_photo data[:photo]
+				  			fullstart = object.send bpi_dictionary['date_debut_collecte']
+				  			xml.date_debut_collecte fullstart.strftime("%F")
+				  			fullend = object.send bpi_dictionary['date_fin_collecte']
+				  			xml.date_fin_collecte fullend.strftime("%F")
+				  			xml.montant_recherche object.send bpi_dictionary['montant_recherche']
+				  			xml.montant_collecte object.send bpi_dictionary['montant_collecte']
+							}
+						elsif xml_stream == "current" && object.try(bpi_dictionary['time_ended?']) == false
+							xml.projet {
+					    	xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
+					    	xml.date_export Time.now.strftime("%Y-%m-%d")
+					    	xml.reference_projet object.send bpi_dictionary['reference_projet']
+								xml.impact_social get_tag(object, 'impact_social')
+								xml.impact_environnemental get_tag(object, 'impact_environnemental')
+								xml.impact_culturel get_tag(object, 'impact_culturel')
+								xml.impact_eco get_tag(object, 'impact_eco')
+								if !get_tag(object, 'categorie1').nil?
+					  			xml.categorie {
+										xml.categorie1 get_tag(object, 'categorie1')
+										xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, 'categorie2').nil?
+					  			}
+								end
+								xml.mots_cles_nomenclature_operateur get_tag(object, 'mots_cles_op') unless get_tag(object, 'mots_cles_op').nil?
+								xml.mode_financement get_tag(object, 'mode_financement')
+								xml.qualif_ESS get_tag(object, 'qualif_ESS')
+								xml.code_postal object.send(bpi_dictionary['code_postal']) unless object.send(bpi_dictionary['code_postal']).empty?
+					  		xml.ville object.send bpi_dictionary['ville']
+					  		xml.titre object.send bpi_dictionary['titre']
+					  		xml.description object.send(bpi_dictionary['description']).gsub(/<\/?[^>]*>/,"") unless object.try(bpi_dictionary['description']).nil?
+					  		xml.url data[:url]
+					  		xml.url_photo data[:photo]
+					  		fullstart = object.send bpi_dictionary['date_debut_collecte']
+					  		xml.date_debut_collecte fullstart.strftime("%F")
+					  		fullend = object.send bpi_dictionary['date_fin_collecte']
+					  		xml.date_fin_collecte fullend.strftime("%F")
+					  		xml.montant_recherche object.send bpi_dictionary['montant_recherche']
+					  		xml.montant_collecte object.send bpi_dictionary['montant_collecte']
+							}
+						end
+					rescue Exception => exception
+						Rails.logger.warn("--------------An error occured in BPI PLUGIN-----------")
+						Rails.logger.warn (exception)
+						Rails.logger.warn("--------------------------------------------------------")
 					end
 				end
 		  }
