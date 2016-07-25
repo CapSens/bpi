521c810e59c23ec3a1233a0a99212c42190ba9eb
Samy KACIMI <samy@capsens.eu>
2016-07-25 16:43:19 +0200

    fix amount type for XML issue

--- lib/bpi/builder.rb
+++ lib/bpi/builder.rb
@@ -20,33 +20,33 @@ module Bpi
 						data = object.get_project_data
 						if xml_stream == "history" && (object.try(bpi_dictionary['success?']) || object.try(bpi_dictionary['failure?']))
 				    	xml.projet {
-				    		xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
-				    		xml.date_export Time.now.strftime("%Y-%m-%d")
-				    		xml.reference_projet object.send bpi_dictionary['reference_projet']
+								xml.reference_partenaire Rails.application.config.bpi.reference_partenaire
+								xml.date_export Time.now.strftime("%Y-%m-%d")
+								xml.reference_projet object.send bpi_dictionary['reference_projet']
 								xml.impact_social get_tag(object, 'impact_social')
 								xml.impact_environnemental get_tag(object, 'impact_environnemental')
 								xml.impact_culturel get_tag(object, 'impact_culturel')
 								xml.impact_eco get_tag(object, 'impact_eco')
 								if !get_tag(object, 'categorie1').nil?
-				  				xml.categorie {
+									xml.categorie {
 										xml.categorie1 get_tag(object, 'categorie1')
-										xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, "categorie2").nil?
-				  				}
+										xml.categorie2 get_tag(object, 'categorie2') unless get_tag(object, 'categorie2').nil?
+									}
 								end
 								xml.mots_cles_nomenclature_operateur get_tag(object, 'mots_cles_op') unless get_tag(object, 'mots_cles_op').nil?
 								xml.mode_financement get_tag(object, 'mode_financement')
 								xml.type_porteur_projet get_tag(object, "type_porteur_projet")
-								xml.qualif_ESS get_tag(object, "qualif_ESS")
+								xml.qualif_ESS get_tag(object, 'qualif_ESS')
 								xml.code_postal get_tag(object, "code_postal")
-				  			xml.ville get_tag(object, "ville")
-				  			xml.titre object.send bpi_dictionary['titre']
-				  			xml.description get_tag(object, 'description').gsub(/<\/?[^>]*>/,"")
-				  			xml.url data[:url]
-				  			xml.url_photo data[:photo]
-				  			xml.date_debut_collecte object.send(bpi_dictionary['date_debut_collecte']).strftime("%F")
-				  			xml.date_fin_collecte object.send(bpi_dictionary['date_fin_collecte']).strftime("%F")
-				  			xml.montant_recherche (object.send bpi_dictionary['montant_recherche']).to_i
-				  			xml.montant_collecte (object.send bpi_dictionary['montant_collecte']).to_i
+								xml.ville get_tag(object, "ville")
+								xml.titre object.send bpi_dictionary['titre']
+								xml.description get_tag(object, 'description').gsub(/<\/?[^>]*>/,"")
+								xml.url data[:url]
+								xml.url_photo data[:photo]
+								xml.date_debut_collecte object.send(bpi_dictionary['date_debut_collecte']).strftime("%F")
+								xml.date_fin_collecte object.send(bpi_dictionary['date_fin_collecte']).strftime("%F")
+								xml.montant_recherche (object.send bpi_dictionary['montant_recherche']).to_s
+								xml.montant_collecte (object.send bpi_dictionary['montant_collecte']).to_s
 							}
 						elsif xml_stream == "current" && object.try(bpi_dictionary['time_ended?']) == false
 							xml.projet {
@@ -75,8 +75,8 @@ module Bpi
 					  		xml.url_photo data[:photo]
 					  		xml.date_debut_collecte object.send(bpi_dictionary['date_debut_collecte']).strftime("%F")
 					  		xml.date_fin_collecte object.send(bpi_dictionary['date_fin_collecte']).strftime("%F")
-					  		xml.montant_recherche (object.send bpi_dictionary['montant_recherche']).to_i
-					  		xml.montant_collecte (object.send bpi_dictionary['montant_collecte']).to_i
+					  		xml.montant_recherche (object.send bpi_dictionary['montant_recherche']).to_s
+					  		xml.montant_collecte (object.send bpi_dictionary['montant_collecte']).to_s
 							}
 						end
 					end
