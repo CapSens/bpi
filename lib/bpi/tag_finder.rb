49cda27effe0ac009fccacc9d646dfa813b76c62
ebardet <eliot@capsens.eu>
2016-12-16 12:05:50 +0100

    fix/get-tag : correct fix

--- lib/bpi/tag_finder.rb
+++ lib/bpi/tag_finder.rb
@@ -3,7 +3,7 @@ module Bpi
   def get_tag(object, key)
     if !bpi_dictionary.has_key?(key) || (bpi_dictionary.has_key?(key) && object.send(bpi_dictionary[key]).nil?)
       bpi_default[key]
-    elsif bpi_dictionary[key].is_a? Hash
+    elsif object.send(bpi_dictionary[key]).is_a? Hash
       # {loc: :localisation}
       object.send(bpi_dictionary[key].keys.first).send(bpi_dictionary[key].values.first)
     else
